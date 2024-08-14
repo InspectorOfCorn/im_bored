0309dd55e135f2f75715e079c550a3f3fd204b4e
Bilal Omar <103400991+InspectorOfCorn@users.noreply.github.com>
2024-08-13 22:53:11 -0700

    Google Maps Integration
    Implemented google maps api into emulator.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAUnwr3q4OaZNrjp0Zx2dBroKIFgd1rzeA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
